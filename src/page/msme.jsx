82e02f9d032332ed465f1317da1f89c4c633eb49
Creticweb <74098049+Creticweb@users.noreply.github.com>
2020-12-21 16:28:43 +0530

    Update msme.jsx

--- src/page/msme.jsx
+++ src/page/msme.jsx
@@ -152,6 +152,143 @@ const MSME = (props) => {
         </div>
       </div>
       <MSMEAL/>
+   <div className="pricing_section padding-b-15 margin-b-15">
+        <div className="container">
+          <div className="row justify-content-md-center">
+
+          </div>
+          <div className="blocks_pricing" id="monthly">
+            <div className="row justify-content-center">
+              <div className="col-md-6 col-lg-4">
+                <div className="item__price">
+                  <div className="logo_price">
+                    <div className="icon_gif">
+                      <img className="gif" src="../../assets/img/gif/partying_face.gif" alt="basic plan" />
+                    </div>
+                  </div>
+                  <h4 className="name_p">Basic</h4>
+                  <div className="number">
+                    <span className="n_price">₹299</span>
+                    <span className="coins">.00</span>
+                    <span className="d-block per">all inclusive fees</span>
+                  </div>
+                  <p className="info_price">
+                    Entire Process Will Be Done in Same Day
+                  </p>
+                  <div className="feature_price">
+                    <ul className="list-group">
+                      <li className="list-group-item">
+                        <i className="tio checkmark_circle_outlined" />
+                       MSME Registration
+                       </li>
+                    </ul>
+                    <a href="https://rzp.io/l/PzbqNVg" type="button" className="btn effect-letter scale rounded-pill btn_md_primary c-white bg-blue">
+                      Order Now!
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6 col-lg-4">
+                <div className="item__price popular">
+                  <img className="icon_popular" src="../../assets/img/popular.svg" alt="Premium Plan" />
+                  <div className="logo_price">
+                    <div className="icon_gif">
+                      <img className="gif" src="../../assets/img/gif/nerd_face.gif" alt="premium" />
+                    </div>
+                  </div>
+                  <h4 className="name_p">Premium</h4>
+                  <div className="number">
+                    <span className="n_price">₹1499</span>
+                    <span className="coins">.00</span>
+                    <span className="d-block per">all inclusive fees</span>
+                  </div>
+                  <p className="info_price">
+                    Entire Process Will Be Done in Same Day
+                  </p>
+                  <div className="feature_price">
+                    <ul className="list-group">
+                      <li className="list-group-item">
+                        <i className="tio checkmark_circle_outlined" />
+                        GST Registration
+                      </li>
+                      <li className="list-group-item">
+                        <i className="tio checkmark_circle_outlined" />
+                        Udyam Registration
+                      </li>
+                      <li className="list-group-item">
+                        <i className="tio checkmark_circle_outlined" />
+                        GST Billing Software
+                      </li>
+                      <li className="list-group-item">
+                        <i className="tio checkmark_circle_outlined" />
+                        Current Account Opening*
+                      </li>
+                      <li className="list-group-item">
+                        <i className="tio checkmark_circle_outlined" />
+                        3Months Free GST Filing
+                      </li>
+                      <li className="list-group-item">
+                        <i className="tio checkmark_circle_outlined" />
+                        Free GST Support
+                      </li>
+                    </ul>
+                    <a href="https://rzp.io/l/GIL4Dcs" type="button" className="btn effect-letter scale rounded-pill btn_md_primary c-white bg-yollow">
+                       Order Now!
+                       </a>
+                    
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6 col-lg-4">
+                <div className="item__price">
+                  <div className="logo_price">
+                    <div className="icon_gif">
+                      <img className="gif" src="../../assets/img/gif/smiling_face_with_sunglasses.gif" alt="Standard"/>
+                    </div>
+                  </div>
+                  <h4 className="name_p">Standard</h4>
+                  <div className="number">
+                    <span className="n_price">₹799</span>
+                    <span className="coins">.00</span>
+                    <span className="d-block per">all inclusive fees</span>
+                  </div>
+                  <p className="info_price">
+                    Entire Process Will Be Done in Same Day
+                  </p>
+                  <div className="feature_price">
+                    <ul className="list-group">
+                      <li className="list-group-item">
+                        <i className="tio checkmark_circle_outlined" />
+                        GST Registration
+                      </li>
+                      <li className="list-group-item">
+                        <i className="tio checkmark_circle_outlined" />
+                        Udyam Registration
+                      </li>
+                      <li className="list-group-item">
+                        <i className="tio checkmark_circle_outlined" />
+                   1Month Free GST Filing
+                      </li>
+                      <li className="list-group-item">
+                        <i className="tio checkmark_circle_outlined" />
+                        Curreun ccountt Opening*
+                      </li>
+                      <li className="list-group-item">
+                        <i className="tio checkmark_circle_outlined" />
+                        GST Billing Software
+                      </li>
+                    </ul>
+                     <a href="https://rzp.io/l/HYXqmSLe" type="button" className="btn scale effect-letter rounded-pill btn_md_primary c-white bg-orange-red">
+                      Order Now!
+                      </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
     <Footer/>
    </React.Fragment>
   );
